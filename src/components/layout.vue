b0a103985ff181ed1011ba3b3c3519f709161eb7
yuanli <2865437316@qq.com>
2024-09-11 14:46:24 +0800

    feat: 封装 layout 组件

---
+++ src/components/layout.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="layout">
+    <div class="header">
+      <div>
+        logo
+      </div>
+      <slot name="header"></slot>
+    </div>
+    <div class="container">
+      <div class="main">
+        <slot name="content"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="less">
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 24px 48px;
+  color: #333;
+  font-size: 20px;
+  font-weight: bold;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, .1);
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+}
+
+.container {
+  flex: 1;
+  background-color: #f0f0f0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.main {
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  max-width: 1280px;
+  padding: 48px;
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-content: center;
+  gap: 16px;
+}
+
+@media (max-width: 768px) {
+  .main {
+    padding: 24px;
+  }
+}
+
+</style>
